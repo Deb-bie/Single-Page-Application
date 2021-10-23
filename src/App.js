7e64665d8e764042664f5189227497de21e2bc31
unknown <Deb-bie>
2021-10-23 23:22:49 +0000

    Modified Discovery.css

--- src/App.js
+++ src/App.js
@@ -1,18 +1,25 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
+import About from './components/About/About';
 import Hero from './components/pages/Hero/Hero';
+import Services from './components/pages/Services/Services';
+import Discovery from './components/pages/Discovery/Discovery';
+import SignUp from './components/pages/Signup/signup';
 
 function App() {
   return (
 
     <Router>
-      <Navbar />
-      <Hero />
-      
+     
       <Switch>
-        <Route path='/' />
+
+        <Route exact path='/' component={Navbar} />
+        <Route path='/signup' component={SignUp} exact />
+
       </Switch>
+
+
     </Router>
   );
 }
